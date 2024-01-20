b1a2c1e4f1efeaaebce7625174892b3c6a7bd525
John Bochicchio <jpbochicchio@gmail.com>
2024-01-20 10:07:44 -0500

    Create initial template

--- src/main.ts
+++ src/main.ts
@@ -53,16 +53,10 @@ bot.on("messageCreate", async (message: Message) => {
 });
 
 async function run() {
-  // The following syntax should be used in the commonjs environment
-  //
-  // await importx(__dirname + "/{events,commands}/**/*.{ts,js}");
-
-  // The following syntax should be used in the ECMAScript environment
   await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
 
-  // Let's start the bot
   if (!process.env.BOT_TOKEN) {
-    throw Error("Could not find BOT_TOKEN in your environment");
+    throw Error("BOT_TOKEN could not be found in the environment variables list");
   }
 
   // Log in with your bot token
